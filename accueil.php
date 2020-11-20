8748b8ad48d8932ab99498dd73e3fbb03666a5f5
choquitofrito <zyriab@gmail.com>
2020-11-20 15:46:39 +0100

    start

---
+++ accueil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        echo "Exemple composer";
+        require "./vendor/autoload.php";
+
+        // importer les namespaces
+        use Monolog\Logger;
+        use Monolog\Handler\StreamHandler;
+
+        // créer un log (ici un fichier dans le disque)
+        $objectLogger = new Logger('monLogger');
+        $objectLogger->pushHandler(new StreamHandler('monLogConnexions.log', Logger::INFO));
+
+        // écrire de messages
+        $objectLogger->info("User xxxxx connecté");
+        $objectLogger->err('Mauvais password');
+
+    ?>
+</body>
+
+</html>
